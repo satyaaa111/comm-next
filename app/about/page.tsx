3c0f006d3e775a43c9895a978d93efc446cc1558
satyaaa111 <juman60000@gmail.com>
2024-04-03 21:43:07 +0530

    Initialized the repo for community app

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='text-[1.9rem]'>
+      About Page
+    </div>
+  )
+}
+
+export default About
---
+++ app/components/ui/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      Hello from home
+    </div>
+  )
+}
+
+export default HomePage
